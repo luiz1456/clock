5e1a0773e0572bf120a2f28a36956be1b11df4c1
luiz1456 <luizrodrigolimadesouza@gmail.com>
2024-04-16 11:06:37 -0300

    add more visual information

--- src/pages/pomodoroTimer/hooks/useCreateTimerConfig.ts
+++ src/pages/pomodoroTimer/hooks/useCreateTimerConfig.ts
@@ -7,22 +7,15 @@ interface Iprops {
 
 export default function useCreateTimerConfig({ focusPeriodSize, shortBreak, longPause, focusPeriods }: Iprops) {
 
-  const focusPeriodsInSeconds = (Number(focusPeriodSize)) || 25 * 60
-  const shortBreakInSeconds = (Number(shortBreak)) || 5 * 60
-  const longPauseInSeconds = (Number(longPause)) || 15 * 60
+
+  const focusPeriodsInSeconds = (Number(focusPeriodSize) * 60) || 25 * 60
+  const shortBreakInSeconds = (Number(shortBreak) * 60) || 5 * 60
+  const longPauseInSeconds = (Number(longPause) * 60) || 15 * 60
   const numberOfFocusPeriods = Number(focusPeriods) || 4
   const timerConfig = { focusPeriodsInSeconds, shortBreakInSeconds, longPauseInSeconds, numberOfFocusPeriods }
   const timerState = JSON.stringify({ currentTime: timerConfig.focusPeriodsInSeconds, currentInterval: 0, isFocusPeriod: true })
   localStorage.setItem('timerConfig', JSON.stringify(timerConfig))
   localStorage.setItem('timerState', timerState)
-  // const focusPeriodsInSeconds = (Number(focusPeriodSize) * 60) || 25 * 60
-  // const shortBreakInSeconds = (Number(shortBreak) * 60) || 5 * 60
-  // const longPauseInSeconds = (Number(longPause) * 60) || 15 * 60
-  // const numberOfFocusPeriods = Number(focusPeriods) || 4
-  // const timerConfig = { focusPeriodsInSeconds, shortBreakInSeconds, longPauseInSeconds, numberOfFocusPeriods }
-  // const timerState = JSON.stringify({ currentTime: timerConfig.focusPeriodsInSeconds, currentInterval: 0, isFocusPeriod: true })
-  // localStorage.setItem('timerConfig', JSON.stringify(timerConfig))
-  // localStorage.setItem('timerState', timerState)
 
   return {
     timerConfig
